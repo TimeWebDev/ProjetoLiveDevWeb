76a090971f76b235a4867a9e6e5d2b659518c64b
lcsilva76 <lsilva@fiap.com.br>
2023-09-16 21:55:03 -0300

    Lista de Produtos puxando do JSON

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 
 export default function Produtos() {
+
+
+  const [produtos, setProdutos]= useState([])
+
+  useEffect(
+    ()=>{
+      fetch("http://localhost:5000/produtos")
+        .then(resp => resp.json())
+        .then(resp => setProdutos(resp))
+        .catch( error => console.log(error))
+    },[]
+  )
+
+  const handleDelete = (id)=>{
+    
+  }
+
+
   return (
-    <div>Produtos</div>
+    <div>
+      <h1>Lista de Jogos</h1>
+
+      <Link to="/cadastrar">Adicionar novo jogo.</Link>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Imagem</th>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Editar / Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            produtos.map(prod => (
+              <tr key={prod.id}>
+                  <td>{prod.img}</td>
+                  <td>{prod.nome}</td>
+                  <td>{prod.desc}</td>
+                  <td>{prod.preco}</td>
+                  <td>
+                    {/* <Link to={`/editar/${prod.id}`}>Editar</Link>
+                    <button onClick={handleDelete.bind(this, prod.id)}>Deletar</button> */}
+                  </td>
+              </tr>
+            ))
+          }
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="5">Lista de Jogos em promoção!!!</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
   )
 }
