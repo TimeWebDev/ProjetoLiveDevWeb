20e39c3216d2240a74b101cae511e008965bbdc7
Wellington <welltecnic@hotmail.com>
2023-09-30 14:16:39 -0300

    Atuliazação do componente Error

--- src/routes/Erro.jsx
+++ src/routes/Erro.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Error from '../img/Error.png'
+import { Link } from 'react-router-dom'
 import styles from '../css/Error.module.css'
 
 export default function Erro() {
   return (
+
     <div>
-      <img src={Error}/>
+    
+      <img src={Error} className={styles.img1} />
+      <Link to="/" className={styles.voltar}>VOLTAR</Link>
+      
     </div>
   )
 }
